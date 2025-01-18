55058b2e0c87d9f3a6afd8b68bf0b0f5104576aa
Simon Matthews (sam) <sam@ac$applicity.co.uk>
2025-01-18 09:57:24 +0000

    fix: Improve error handling

--- report.js
+++ report.js
@@ -7,7 +7,7 @@ const cron = require('node-cron');
 
 const { combine, timestamp, printf } = winston.format;
 const logger = winston.createLogger({
-  level: 'info',
+  level: 'debug',
   format: combine(
     winston.format.errors({ stack: true }), // log the full stack
     timestamp(), // get the time stamp part of the full log message
@@ -44,45 +44,57 @@ const reportData = (variables) => {
   //   console.log(r);
   //   return r;
   // })
-  .then(data => logger.log('debug', `data returned: ${JSON.stringify(data)}`));
+  .then(data => logger.log('debug', `data returned: ${JSON.stringify(data)}`))
+  .catch((err) => {
+    logger.error('Failed to report data', err)
+  });
 }
 
 const getDetails = (ip) => {
 
   return new Promise((res, rej) => {
 
-    var client = new MediaRendererClient(`http://${ip}:49152/description.xml`);
-    client.getVolume(function(err, volume) {
-      if(err) return rej(err);
-
-      client.getTransportInfo(function(err2, transportInfo) {
-        if(err2) return rej(err2);
-
-        client.callAction('AVTransport', 'GetMediaInfo', { InstanceID: 0 }, function(err3, media) {
-          if(err3) return rej(err3);
-
-
-          client.callAction(
-            "RenderingControl",
-            // "urn:upnp-org:serviceId:RenderingControl",
-            "GetControlDeviceInfo",
-            { InstanceID: 0 },
-            function (err4, result) {
-              if (err4) return rej(err4);
-              const statusText = result.Status
-              const status = JSON.parse(statusText)
-              // console.log(status)
-              // console.log(result); // => { NrTracks: '1', MediaDuration: ... }
-              return res({volume, transportInfo, media, deviceStatus: status })
-            }
-          );
-
+    try {
+      logger.debug(`creating MediaRenderClient for ${ip}`)
+      var client = new MediaRendererClient(`http://${ip}:49152/description.xml`);
+      logger.debug(`created MediaRenderClient for ${ip}`)
+      client.getVolume(function(err, volume) {
+        if(err) return rej(err);
+        logger.debug('Got volume')
+  
+        client.getTransportInfo(function(err2, transportInfo) {
+          if(err2) return rej(err2);
+  
+          client.callAction('AVTransport', 'GetMediaInfo', { InstanceID: 0 }, function(err3, media) {
+            if(err3) return rej(err3);
+    
+            client.callAction(
+              "RenderingControl",
+              // "urn:upnp-org:serviceId:RenderingControl",
+              "GetControlDeviceInfo",
+              { InstanceID: 0 },
+              function (err4, result) {
+                if (err4) return rej(err4);
+                const statusText = result.Status
+                const status = JSON.parse(statusText)
+                // console.log(status)
+                // console.log(result); // => { NrTracks: '1', MediaDuration: ... }
+                logger.debug('Got device status', {status})
+                return res({volume, transportInfo, media, deviceStatus: status })
+              }
+            );
+  
+          });
+  
         });
-
+  
+  
       });
-
-
-    });
+  
+    } catch (error) {
+      logger.error('Failed to get details', error)
+      res()
+    }
 
   })
 
@@ -138,23 +150,34 @@ upnp.on('added', async (device) => {
 
 const reportAllData = async () => {
   // console.log('Sending reports', reports.length);
+  logger.info(`Sending reports ${reports.length}`)
 
   let count = 0;
   for (const report of reports) {
-    console.log('Reporting', report);
-    const details = await getDetails(report.ip);
+    logger.debug(`Reporting data for ${report.ip}`)
+    // console.log('Reporting', report);
+    try {
+      const details = await getDetails(report.ip);
 
-    const { volume, transportInfo, media, deviceStatus } = details;
-
-    const state = transportInfo.CurrentTransportState;
-    const status = transportInfo.CurrentTransportStatus;
-    const {CurrentURI: uri} = media;
-
-          // reports.push({name, radio_type, ip, volume, status, state, uri});
-
-    const res = await reportData({...report, volume, status, state, uri, rssi: deviceStatus?.RSSI});
-    // console.log('Res', res);
-    count += 1;
+      if (details) {
+        const { volume, transportInfo, media, deviceStatus } = details;
+  
+        const state = transportInfo.CurrentTransportState;
+        const status = transportInfo.CurrentTransportStatus;
+        const {CurrentURI: uri} = media;
+    
+        // reports.push({name, radio_type, ip, volume, status, state, uri});
+    
+        const res = await reportData({...report, volume, status, state, uri, rssi: deviceStatus.RSSI});
+        // console.log('Res', res);
+        count += 1;  
+      } else {
+        logger.info(`Failed to get details for ${report.ip}`)
+      }
+  
+    } catch (error) {
+      logger.error(`Failed to report for ${report.ip}`)
+    }
   }
   // clear out the reports after we sent them.
   reports = [];
@@ -168,7 +191,7 @@ const discoveryProcess = () => {
     upnp.startDiscovery();
     logger.info('Started discovery');
 
-    // Stop the discovery process in 15 seconds
+    // Stop the discovery process in 20 seconds
     setTimeout(() => {
       upnp.stopDiscovery(async () => {
         logger.info('Stopped the discovery process.');
@@ -192,6 +215,19 @@ const schedule = '0,10,20,30,40,50 * * * *';
 cron.schedule(schedule, () => {
   discoveryProcess();
 });
+
+// const test = async () => {
+
+//   try {
+//     await getDetails('192.168.10.215')
+  
+//   } catch (err) {
+//     console.log('Caught', err)
+//   }
+  
+// }
+
+// test()
 
 // discoveryProcess();
 
